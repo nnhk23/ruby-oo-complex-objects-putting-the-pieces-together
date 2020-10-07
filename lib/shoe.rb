5ae2bcbfd9a3db66743ae1755a93b634104ab556
Kim Nguyen <nnhk23@gmail.com>
2020-10-06 18:38:54 -0700

    Input shoe information

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,20 @@
-# Make your shoe class here!
+class Shoe
+attr_reader :brand, :cobble
+attr_accessor :color, :size, :material, :condition
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+
+end
+
+# shoe = Shoe.new("Nike Airforce")
+# puts shoe.brand
+# puts shoe.size="8"
+# puts shoe.condition="Old"
+# shoe.cobble
